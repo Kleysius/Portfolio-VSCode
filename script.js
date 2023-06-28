ccf71913c4f25037858f2ce751f57d4673f497ab
Thomas <sebasti.thomass@gmail.com>
2023-06-28 11:39:14 +0200

    cleaning up the code

--- script.js
+++ script.js
@@ -1,8 +1,77 @@
 let mailIsLoading = false;
+const backgrounds = [
+    'url(./assets/img/fdwin11-1.jpeg)',
+    'url(./assets/img/fdwin11-2.jpg)',
+    'url(./assets/img/fdwin11-3.jpg)',
+    'url(./assets/img/fdwin11-4.jpeg)',
+    'url(./assets/img/fdwin11-5.jpg)',
+    'url(./assets/img/fdwin11-6.jpg)',
+    ''
+];
+let currentBackground = 0;
+
+// Constantes pour la fenêtre
+const container = document.getElementById('container');
+const content = document.querySelector('.content');
+const logoVscode = document.querySelector('.logo-vscode');
+const closeBtn = document.querySelector('.close-button');
+const minimize = document.querySelector('.minimize-button');
+const maximize = document.querySelector('.maximize-button');
+const titleBar = document.querySelector('.title-bar');
+let isDragged = false;
+let offsetX, offsetY;
+let isMaxWidthWindow = false;
+
+// Constantes pour les menus
+const menuLinks = document.querySelectorAll('.aLink');
+const menuItem = document.querySelector('.context-menu-item');
+
+// Constantes pour la section active
+const sections = document.querySelectorAll('section');
+
+// Constantes pour le basculement de la liste de fichiers
+const toggleChevron = document.querySelector('.toggle-chevron');
+const fileList = document.querySelector('.explorer-content');
+
+// Constantes pour le thème VSCode
+const gearIcon = document.querySelector('.gear-icon');
+const themeVscodeNameLight = document.querySelector('.theme-vscode-name.light');
+const themeVscodeNameDark = document.querySelector('.theme-vscode-name.dark');
+const themeVscodeNameDracula = document.querySelector('.theme-vscode-name.dracula');
+const themeModal = document.querySelector('.theme-modal');
+
+// Constante pour le bouton de chargement et le loader
+const loaderContainer = document.getElementById("loader-container");
+const loaderBtn = document.querySelector(".loaderBtn");
+loaderBtn.style.display = "none";
+
+// Constantes pour le formulaire de contact
+const btnMail = document.querySelector("#btnMail");
+const nameInput = document.querySelector("#contact_nom");
+const emailInput = document.querySelector("#contact_email");
+const messageInput = document.querySelector("#contact_message");
+const subjectInput = document.querySelector("#contact_sujet");
+
+// Constantes pour les messages de succès/erreur
+const successMessage = document.querySelector("#success-message");
+const errorMessage = document.querySelector("#error-message");
+
+// Constantes pour la modale de chargement
+const modalBg = document.getElementById('customModal');
+
+// Constantes pour les jeux
+const morpionIcon = document.getElementById('morpion-icon');
+const puissance4Icon = document.getElementById('puissance4-icon');
+const calculatriceIcon = document.getElementById('calculatrice-icon');
+const gameContainer = document.getElementById('game-container');
+const gameIframe = document.getElementById('game-iframe');
+const closeButton = document.getElementById('close-button-game');
+
+// Constante pour la date et l'heure actuelles
+const dateElement = document.querySelector('.date');
+const timeElement = document.querySelector('.time');
 
 window.addEventListener("load", function () {
-    const loaderContainer = document.getElementById("loader-container");
-
     setTimeout(function () {
         loaderContainer.style.backdropFilter = "blur(0)";
         setTimeout(function () {
@@ -24,7 +93,6 @@ document.addEventListener('contextmenu', function (event) {
     let mouseY = event.clientY;
 
     // Positionnement de la modale
-    const modalBg = document.getElementById('customModal');
     modalBg.style.top = mouseY + 'px';
     modalBg.style.left = mouseX + 'px';
     modalBg.style.display = 'block';
@@ -37,72 +105,36 @@ document.addEventListener('click', function (event) {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const menuItem = document.querySelector('.context-menu-item');
-    const morpionIcon = document.getElementById('morpion-icon');
-    const puissance4Icon = document.getElementById('puissance4-icon');
-    const calculatriceIcon = document.getElementById('calculatrice-icon');
-    const gameContainer = document.getElementById('game-container');
-    const gameIframe = document.getElementById('game-iframe');
-    const closeButton = document.getElementById('close-button-game');
-
     morpionIcon.addEventListener('click', openMorpion);
     puissance4Icon.addEventListener('click', openPuissance4);
     calculatriceIcon.addEventListener('click', openCalculatrice);
     closeButton.addEventListener('click', closeGame);
+    menuItem.addEventListener('click', changeBackground);
 
     function openMorpion() {
         gameContainer.style.display = 'block';
         gameIframe.src = 'https://kleysius.github.io/Morpion/';
-        gameContainer.style.width = '';
-        gameContainer.style.height = '';
-        closeButton.style.top = '';
-        closeButton.style.right = '';
-        closeButton.style.fontSize = '';
+        [gameContainer.style.width, gameContainer.style.height, closeButton.style.top, closeButton.style.right, closeButton.style.fontSize] = ['', '', '', '', ''];
     }
 
     function openPuissance4() {
         gameContainer.style.display = 'block';
         gameIframe.src = 'https://kleysius.github.io/Puissance-4/';
-        gameContainer.style.width = '';
-        gameContainer.style.height = '';
-        closeButton.style.top = '';
-        closeButton.style.right = '';
-        closeButton.style.fontSize = '';
+        [gameContainer.style.width, gameContainer.style.height, closeButton.style.top, closeButton.style.right, closeButton.style.fontSize] = ['', '', '', '', ''];
     }
 
     function openCalculatrice() {
         gameContainer.style.display = 'block';
         gameIframe.src = 'https://kleysius.github.io/calculatrice/';
-        gameContainer.style.width = '300px';
-        gameContainer.style.height = '410px';
-        closeButton.style.top = '5px';
-        closeButton.style.right = '8px';
-        closeButton.style.fontSize = '20px';
+        [gameContainer.style.width, gameContainer.style.height, closeButton.style.top, closeButton.style.right, closeButton.style.fontSize] = ['300px', '410px', '5px', '8px', '20px'];
     }
 
     function closeGame() {
         gameContainer.style.display = 'none';
-        gameIframe.src = '';
-        gameContainer.style.width = '';
-        gameContainer.style.height = '';
-        closeButton.style.top = '';
-        closeButton.style.right = '';
-        closeButton.style.fontSize = '';
+        [gameIframe.src, gameContainer.style.width, gameContainer.style.height, closeButton.style.top, closeButton.style.right, closeButton.style.fontSize] = ['', '', '', '', '', ''];
     }
 
-    menuItem.addEventListener('click', changeBackground);
 });
-
-const backgrounds = [
-    'url(./assets/img/fdwin11-1.jpeg)',
-    'url(./assets/img/fdwin11-2.jpg)',
-    'url(./assets/img/fdwin11-3.jpg)',
-    'url(./assets/img/fdwin11-4.jpeg)',
-    'url(./assets/img/fdwin11-5.jpg)',
-    'url(./assets/img/fdwin11-6.jpg)',
-    ''
-];
-let currentBackground = 0;
 
 function changeBackground() {
     document.body.style.backgroundImage = backgrounds[currentBackground];
@@ -110,19 +142,6 @@ function changeBackground() {
 }
 
 // Faire en sorte de pouvoir déplacer la fenêtre
-// 1. Récupérer la fenêtre
-const container = document.getElementById('container');
-const content = document.querySelector('.content');
-const logoVscode = document.querySelector('.logo-vscode');
-const closeBtn = document.querySelector('.close-button');
-const minimize = document.querySelector('.minimize-button');
-const maximize = document.querySelector('.maximize-button');
-const titleBar = document.querySelector('.title-bar');
-let isDragged = false;
-let offsetX, offsetY;
-let isMaxWidthWindow = false;
-
-// 2. Ajouter un event listener sur la titleBar
 titleBar.addEventListener('mousedown', function (e) {
     isDragged = true;
     offsetX = e.clientX - container.offsetLeft;
@@ -148,32 +167,8 @@ document.addEventListener('mouseup', function (e) {
     isDragged = false;
 });
 
-maximize.addEventListener('click', function () {
-    if (!isMaxWidthWindow) {
-        container.style.width = '100vw';
-        container.style.height = 'calc(100vh - 60px)';
-        container.style.top = '0';
-        container.style.left = '0';
-        container.style.border = 'none';
-        container.style.borderRadius = 0;
-        container.style.transition = 'width 0.3s ease, height 0.3s ease, border-radius 0.3s ease';
-        maximize.innerHTML = '<i class="fa-solid fa-window-restore"></i>';
-        isMaxWidthWindow = true;
-    } else {
-        container.style.width = '1000px';
-        container.style.height = '650px';
-        container.style.borderRadius = '10px';
-        container.style.transition = 'width 0.3s ease, height 0.3s ease, border-radius 0.3s ease';
-        maximize.innerHTML = '<i class="fa-regular fa-square"></i>';
-        isMaxWidthWindow = false;
-    }
-});
-
 // Heure et date actuelle
 function updateTime() {
-    const dateElement = document.querySelector('.date');
-    const timeElement = document.querySelector('.time');
-
     const currentDate = new Date();
     const date = currentDate.toLocaleDateString('fr-FR', {
         day: '2-digit',
@@ -220,8 +215,26 @@ minimize.addEventListener('click', function () {
     container.style.transition = 'opacity 0.3s ease';
 });
 
-// Récupère tous les liens de la barre de menu
-const menuLinks = document.querySelectorAll('.aLink');
+maximize.addEventListener('click', function () {
+    if (!isMaxWidthWindow) {
+        container.style.width = '100vw';
+        container.style.height = 'calc(100vh - 60px)';
+        container.style.top = '0';
+        container.style.left = '0';
+        container.style.border = 'none';
+        container.style.borderRadius = 0;
+        container.style.transition = 'width 0.3s ease, height 0.3s ease, border-radius 0.3s ease';
+        maximize.innerHTML = '<i class="fa-solid fa-window-restore"></i>';
+        isMaxWidthWindow = true;
+    } else {
+        container.style.width = '1000px';
+        container.style.height = '650px';
+        container.style.borderRadius = '10px';
+        container.style.transition = 'width 0.3s ease, height 0.3s ease, border-radius 0.3s ease';
+        maximize.innerHTML = '<i class="fa-regular fa-square"></i>';
+        isMaxWidthWindow = false;
+    }
+});
 
 // Ajoute un gestionnaire d'événement de clic à chaque lien de la barre de menu
 menuLinks.forEach(link => {
@@ -238,77 +251,42 @@ menuLinks.forEach(link => {
 content.addEventListener('scroll', () => {
     const scrollPosition = content.scrollTop;
     const sections = document.querySelectorAll('section');
+
     sections.forEach(section => {
-        const { offsetTop, offsetHeight } = section;
+        const { offsetTop, offsetHeight, id } = section;
+        const menuLink = document.querySelector(`.menu-bar a[href="#${id}"]`);
+        const explorerLink = document.querySelector(`.file-list li a[href="#${id}"]`);
 
         if (scrollPosition >= offsetTop && scrollPosition < offsetTop + offsetHeight) {
-            const id = section.getAttribute('id');
-            const menuLink = document.querySelector(`.menu-bar a[href="#${id}"]`);
-            const explorerLink = document.querySelector(`.file-list li a[href="#${id}"]`);
             menuLink.classList.add('active');
             explorerLink.classList.add('active');
         } else {
-            const id = section.getAttribute('id');
-            const menuLink = document.querySelector(`.menu-bar a[href="#${id}"]`);
-            const explorerLink = document.querySelector(`.file-list li a[href="#${id}"]`);
             menuLink.classList.remove('active');
             explorerLink.classList.remove('active');
         }
     });
 });
 
-
-// Sélectionnez le chevron et la liste de fichiers
-const toggleChevron = document.querySelector('.toggle-chevron');
-const fileList = document.querySelector('.explorer-content');
-
 // Affichage de la liste des fichiers
 toggleChevron.addEventListener('click', function () {
     fileList.classList.toggle('show');
 });
 
-const gearIcon = document.querySelector('.gear-icon');
-
 gearIcon.addEventListener('click', () => {
     themeModal.classList.toggle('active');
 });
 
-const themeVscodeNameLight = document.querySelector('.theme-vscode-name.light');
-const themeVscodeNameDark = document.querySelector('.theme-vscode-name.dark');
-const themeVscodeNameDracula = document.querySelector('.theme-vscode-name.dracula');
-const themeModal = document.querySelector('.theme-modal');
-
-themeVscodeNameLight.addEventListener('click', () => {
-    container.classList.remove('dark');
-    container.classList.remove('dracula')
-    container.classList.add('light');
+const applyTheme = (theme) => {
+    container.classList.remove('light', 'dark', 'dracula');
+    container.classList.add(theme);
     themeModal.classList.remove('active');
-});
+};
 
-themeVscodeNameDark.addEventListener('click', () => {
-    container.classList.remove('light');
-    container.classList.remove('dracula')
-    container.classList.add('dark');
-    themeModal.classList.remove('active');
-});
-
-themeVscodeNameDracula.addEventListener('click', () => {
-    container.classList.remove('light');
-    container.classList.remove('dark');
-    container.classList.add('dracula');
-    themeModal.classList.remove('active');
-});
-
-let loaderBtn = document.querySelector(".loaderBtn");
-loaderBtn.style.display = "none";
+themeVscodeNameLight.addEventListener('click', () => applyTheme('light'));
+themeVscodeNameDark.addEventListener('click', () => applyTheme('dark'));
+themeVscodeNameDracula.addEventListener('click', () => applyTheme('dracula'));
 
 async function sendMail() {
-    let btnMail = document.querySelector("#btnMail");
-    let name = document.querySelector("#contact_nom").value;
-    let email = document.querySelector("#contact_email").value;
-    let message = document.querySelector("#contact_message").value;
-    let subject = document.querySelector("#contact_sujet").value;
-
     if (!mailIsLoading) {
         mailIsLoading = true;
         btnMail.classList.add('loading');
@@ -321,10 +299,10 @@ async function sendMail() {
         };
 
         let body = JSON.stringify({
-            name: name,
-            email: email,
-            message: message,
-            subject: subject
+            name: nameInput,
+            email: emailInput,
+            message: messageInput,
+            subject: subjectInput
         });
 
         let options = {
@@ -336,13 +314,10 @@ async function sendMail() {
         let response = await fetch('https://sebastithomas.fr/contactApi', options);
 
         if (response.status == 200) {
-            document.querySelector("#success-message").innerHTML = "Votre message a bien été envoyé !";
-            name = "";
-            email = "";
-            message = "";
-            subject = "";
+            successMessage.innerHTML = "Votre message a bien été envoyé !";
+            [nameInput, emailInput, messageInput, subjectInput] = ["", "", "", ""];
         } else {
-            document.querySelector("#error-message").innerHTML = "Une erreur est survenue lors de l'envoi du mail.";
+            errorMessage.innerHTML = "Une erreur est survenue lors de l'envoi du mail.";
         }
         btnMail.innerHTML = "Envoyer";
         loaderBtn.style.display = "none";
