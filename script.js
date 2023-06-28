f22f246af2a31b7d6961b47e4113483b178d2de8
Thomas <sebasti.thomass@gmail.com>
2023-06-28 12:17:47 +0200

    problem send mail

--- script.js
+++ script.js
@@ -48,6 +48,13 @@ const loaderContainer = document.getElementById("loader-container");
 const loaderBtn = document.querySelector(".loaderBtn");
 loaderBtn.style.display = "none";
 
+// Constantes pour le formulaire de contact
+const btnMail = document.querySelector("#btnMail");
+const nameInput = document.querySelector("#contact_nom").value;
+const emailInput = document.querySelector("#contact_email").value;
+const subjectInput = document.querySelector("#contact_sujet").value;
+const messageInput = document.querySelector("#contact_message").value;
+
 // Constantes pour les messages de succès/erreur
 const successMessage = document.querySelector("#success-message");
 const errorMessage = document.querySelector("#error-message");
@@ -282,13 +289,6 @@ themeVscodeNameDark.addEventListener('click', () => applyTheme('dark'));
 themeVscodeNameDracula.addEventListener('click', () => applyTheme('dracula'));
 
 async function sendMail() {
-    // Constantes pour le formulaire de contact
-    const btnMail = document.querySelector("#btnMail");
-    let nameInput = document.querySelector("#contact_nom").value;
-    let emailInput = document.querySelector("#contact_email").value;
-    let messageInput = document.querySelector("#contact_message").value;
-    let subjectInput = document.querySelector("#contact_sujet").value;
-
     if (!mailIsLoading) {
         mailIsLoading = true;
         btnMail.classList.add('loading');
@@ -315,25 +315,18 @@ async function sendMail() {
 
         let response = await fetch('https://sebastithomas.fr/contactApi', options);
 
-        if (response.ok) {
-            let data = await response.json();
-            if (data.status === 'success') {
-                // vider les champs
-                nameInput = "";
-                emailInput = "";
-                subjectInput = "";
-                messageInput = "";
-                // afficher le message de succès
-                btnMail.innerHTML = "Envoyé !";
-                btnMail.classList.remove('loading');
-                loaderBtn.style.display = "none";
-                btnMail.disabled = false;
-                mailIsLoading = false;
-            } else {
-                btnMail.innerHTML = "Erreur lors de l'envoi";
-            }
+        if (response.status == 200) {
+            successMessage.innerHTML = "Votre message a bien été envoyé !";
+            nameInput = "";
+            emailInput = "";
+            messageInput = "";
+            subjectInput = "";
         } else {
-            btnMail.innerHTML = "Erreur lors de l'envoi";
+            errorMessage.innerHTML = "Une erreur est survenue lors de l'envoi du mail.";
         }
+        btnMail.innerHTML = "Envoyer";
+        loaderBtn.style.display = "none";
+        btnMail.classList.remove('loading');
+        mailIsLoading = false;
     }
 }
